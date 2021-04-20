e6b138f58e2268e6d1bc2119f67ab3b74ade50b6
Zim Agu <zimmagu@gmail.com>
2021-04-20 15:06:46 -0700

    Added an elapsed time variable to the snake javascript file

---
+++ snake.js
@@ -0,0 +1,21 @@
+// Written by: Zimm Agu
+
+
+let previous_Render_Time = 0 // This variable is needed to get the elapsed time bewteen each render
+let snake_Speed = 2
+
+function snake_Game_Loop (current_Render_Time) {
+
+    window.requestAnimationFrame(snake_Game_Loop) // Recursively calls the itself so the Animation frame can constantly be updated
+
+    let elapsed_Render_Time_In_Seconds = (current_Render_Time - previous_Render_Time) / 1000
+
+
+
+
+    previous_Render_Time = current_Render_Time // every time the loop ends, the current time becomes the previous time
+
+    console.log(elapsed_Render_Time_In_Seconds)
+}
+
+window.requestAnimationFrame(snake_Game_Loop);  // Starts the game loop initially
