66d6f299b71727bdf6bc46be9bc90082d2af3c04
Zim Agu <zimmagu@gmail.com>
2021-05-11 21:09:19 -0700

    Snake controls finished

--- snake.js
+++ snake.js
@@ -1,6 +1,6 @@
 // Written by: Zim Agu
 
-export let snake_Moves_Per_Second = 2;
+export let snake_Moves_Per_Second = 5;
 export let snake_Speed = 1 / snake_Moves_Per_Second; // If the snake moves 2 times per second, 1/2 means there is 0.5 seconds between each move
 import {user_Moves_The_Snake} from "./user_controls.js"
 
--- user_controls.js
+++ user_controls.js
@@ -8,33 +8,49 @@ window.addEventListener('keydown', event => { // These are the user controls
     switch (event.key) {
         // If the user presses the up arrow or w, the snake will move up
         case 'ArrowUp':
+            if (input_Direction.y != 0) // If the snake is moving up or down, then the up control will not work
+                break;
             input_Direction = {x: 0, y: -1};
             break;
         case 'w':
+            if (input_Direction.y != 0)
+                break;
             input_Direction = {x: 0, y: -1};
             break;
 
         // If the user presses the left arrow or a, the snake will move left
         case 'ArrowLeft':
+            if (input_Direction.x != 0)  // If the snake is moving left or right, then left control will not work
+                break;
             input_Direction = {x: -1, y: 0};
             break;
         case 'a':
+            if (input_Direction.x != 0)
+                break;
             input_Direction = {x: -1, y: 0};
             break;
 
         // If the user presses the down arrow or s, the snake will move down
         case 'ArrowDown':
+            if (input_Direction.y != 0)  // If the snake is moving up or down, then the down control will not work
+                break;
             input_Direction = {x: 0, y: 1};
             break;
         case 's':
+            if (input_Direction.y != 0)
+                break;
             input_Direction = {x: 0, y: 1};
             break;
 
         // If the user presses the right arrow or d, the snake will move right
         case 'ArrowRight':
+            if (input_Direction.x != 0) // If the snake is moving left or right, then the right control will not work
+                break;
             input_Direction = {x: 1, y: 0};
             break;
         case 'd':
+            if (input_Direction.x != 0)
+                break;
             input_Direction = {x: 1, y: 0};
             break;
     }
